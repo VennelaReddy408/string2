69b590b51999f1595312b8c0b58ed7f7740be5ef
VennelaReddy408 <podduturivennela@gmail.com>
2024-11-11 21:18:48 +0530

    Add files via upload

---
+++ stringspart2.js
@@ -0,0 +1,20 @@
+
+// Relace the substring “World!” with “My Dear Friend” ..
+let str = "Hello World!";
+console.log(str.replace("World","My dear friend"))
+
+// Repeat the “Hungry” for 10 times
+let a= "Hungry ";
+console.log(a.repeat(10));
+
+// Find the last index of the string “fox”
+let sentence = "The quick brown fox jumps over the lazy dog. The fox is clever."
+console.log(sentence.lastIndexOf("fox"));
+
+// Find the string starts with “hi”
+let message = "Hello, world!";
+console.log(message.startsWith("hi"));
+
+// Find the string ends with “.pdf”
+let fileName = "document.pdf";
+console.log(fileName.endsWith(".pdf"));
